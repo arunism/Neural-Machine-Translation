483ec384fec99f7e51ffa809e47227c419d2312d
arunism <thearunism@gmail.com>
2022-06-07 00:42:33 +0545

    Refactored code

--- models/base.py
+++ models/base.py
@@ -3,6 +3,13 @@ import torch.nn as nn
 class BaseEncoder(nn.Module):
     def __init__(self, config, input_size) -> None:
         super(BaseEncoder, self).__init__()
+        self.config = config
+        self.input_size = input_size
+        self.embed_size = self.config.EMBED_SIZE
+        self.layers_count = self.config.LAYERS_COUNT
+        self.hidden_size = self.config.HIDDEN_SIZE
+        self.dropout = self.config.DROPOUT
+        self.embed_layer = nn.Embedding(self.input_size, self.embed_size)
     
     def forward(self, x):
         raise NotImplementedError
@@ -11,6 +18,14 @@ class BaseEncoder(nn.Module):
 class BaseDecoder(nn.Module):
     def __init__(self, config, output_size) -> None:
         super(BaseDecoder, self).__init__()
+        self.config = config
+        self.input_size = input_size
+        self.output_size = output_size
+        self.embed_size = self.config.EMBED_SIZE
+        self.layers_count = self.config.LAYERS_COUNT
+        self.hidden_size = self.config.HIDDEN_SIZE
+        self.dropout = self.config.DROPOUT
+        self.embed_layer = nn.Embedding(self.input_size, self.embed_size)
     
     def forward(self, x, hidden_state, cell_state):
         raise NotImplementedError
--- models/lstm.py
+++ models/lstm.py
@@ -5,16 +5,7 @@ from models.base import BaseEncoder, BaseDecoder
 class Encoder(BaseEncoder):
     def __init__(self, config, input_size) -> None:
         super(Encoder, self).__init__(config, input_size)
-        self.config = config
-        self.input_size = input_size
-        self.embed_size = self.config.EMBED_SIZE
-        self.layers_count = self.config.LAYERS_COUNT
-        self.hidden_size = self.config.HIDDEN_SIZE
-        self.dropout = self.config.DROPOUT
-
-        self.embed_layer = nn.Embedding(self.input_size, self.embed_size)
         self.lstm = nn.LSTM(self.embed_size, self.hidden_size, self.layers_count, dropout=self.dropout)
-        print(self.lstm)
     
     def forward(self, x):
         embedding = self.dropout(self.embed_layer(x))
@@ -25,15 +16,6 @@ class Encoder(BaseEncoder):
 class Decoder(BaseDecoder):
     def __init__(self, config, input_size, output_size) -> None:
         super(Decoder, self).__init__(config, output_size)
-        self.config = config
-        self.input_size = input_size
-        self.output_size = output_size
-        self.embed_size = self.config.EMBED_SIZE
-        self.layers_count = self.config.LAYERS_COUNT
-        self.hidden_size = self.config.HIDDEN_SIZE
-        self.dropout = self.config.DROPOUT
-
-        self.embed_layer = nn.Embedding(self.input_size, self.embed_size)
         self.lstm = nn.LSTM(self.embed_size, self.hidden_size, self.layers_count, dropout=self.dropout)
         self.fc = nn.Linear(self.hidden_size, self.output_size)
     
