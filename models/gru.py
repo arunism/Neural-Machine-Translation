0645046d15057d1b8319b456c601cab363bca576
arunism <thearunism@gmail.com>
2022-07-03 14:31:07 +0545

    GRU decoder

--- models/gru.py
+++ models/gru.py
@@ -1,8 +1,5 @@
-import random
-import torch
 import torch.nn as nn
-import torch.nn.functional as F
-from models.base import BaseEncoder, BaseDecoder, BaseModel
+from models.base import BaseEncoder, BaseDecoder
 
 class GruEncoder(BaseEncoder):
     def __init__(self, config, input_size) -> None:
@@ -13,3 +10,18 @@ class GruEncoder(BaseEncoder):
         embedding = self.dropout(self.embedding(x))
         output, (hidden, cell) = self.gru(embedding)
         return hidden, cell
+
+
+class GruDecoder(BaseDecoder):
+    def __init__(self, config, input_size, output_size) -> None:
+        super(GruDecoder, self).__init__(config, input_size, output_size)
+        self.gru = nn.GRU(self.embed_size, self.hidden_size, self.layers_count, dropout=self.config.DROPOUT)
+        self.fc = nn.Linear(self.hidden_size, self.output_size)
+    
+    def forward(self, x, hidden, cell):
+        x = x.unsqueeze(0)
+        embedding = self.dropout(self.embedding(x))
+        output, (hidden, cell) = self.gru(embedding, (hidden, cell))
+        prediction = self.fc(output)
+        prediction = prediction.squeeze(0)
+        return prediction, hidden, cell
