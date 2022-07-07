0e8f4dea832fdccf4b6ba63c750c0eb558afd7d1
arunism <thearunism@gmail.com>
2022-07-07 19:01:56 +0545

    BiLstm Decoder

--- models/bilstm.py
+++ models/bilstm.py
@@ -34,4 +34,24 @@ class Attention(nn.Module):
         hidden = hidden.repeat(src_len, 1, 1)
         energy = self.relu(self.energy(torch.cat((hidden, output), dim=2)))
         attention = self.softmax(energy)
-        return attention.permute(1, 0, 2)
+        return attention.permute(1, 2, 0)
+
+
+class LstmAttenDecoder(BaseDecoder):
+    def __init__(self, config, input_size, output_size) -> None:
+        super(LstmAttenDecoder, self).__init__(config, input_size, output_size)
+        self.attention = Attention(self.config)
+        self.lstm = nn.LSTM(self.embed_size, self.hidden_size, self.layers_count, dropout=self.config.DROPOUT)
+        self.fc = nn.Linear(self.hidden_size, self.output_size)
+    
+    def forward(self, input, output, hidden, cell):
+        input = input.unsqueeze(0)
+        embedding = self.dropout(self.embedding(input))
+        attention = self.attention(output, hidden, cell)
+        output = output.permute(1, 0, 2)
+        context_vector = torch.bmm(attention, output).permute(1, 0, 2)
+        input = torch.cat((context_vector, embedding), dim=2)
+        output, (hidden, cell) = self.lstm(input, (hidden, cell))
+        prediction = self.fc(output)
+        prediction = prediction.squeeze(0)
+        return prediction, hidden, cell
