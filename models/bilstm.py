55da0bf6e1ae0a31e1e8045f4b4e33b1567de308
arunism <thearunism@gmail.com>
2022-07-07 17:17:52 +0545

    BiLstm attention

--- models/bilstm.py
+++ models/bilstm.py
@@ -1,4 +1,5 @@
 import random
+from turtle import forward
 from unicodedata import bidirectional
 import torch
 import torch.nn as nn
@@ -17,3 +18,20 @@ class LstmAttenEncoder(BaseEncoder):
         hidden = self.fc(torch.cat((hidden[0:1], hidden[1:2]), dim=2))
         cell = self.fc(torch.cat((cell[0:1], cell[1:2]), dim=2))
         return output, hidden, cell
+
+
+class Attention(nn.Module):
+    def __init__(self, config) -> None:
+        super().__init__()
+        self.config = config
+        self.hidden_size = self.config.HIDDEN_SIZE
+        self.energy = nn.Linear(self.hidden_size*3, 1)
+        self.relu = nn.ReLU()
+        self.softmax = nn.Softmax(dim=0)
+    
+    def forward(self, output, hidden, cell):
+        src_len = output.size(0)
+        hidden = hidden.repeat(src_len, 1, 1)
+        energy = self.relu(self.energy(torch.cat((hidden, output), dim=2)))
+        attention = self.softmax(energy)
+        return attention.permute(1, 0, 2)
